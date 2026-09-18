1b346b77c2c5a69cd696e4d6299d47d73a44bcbd
Rith0068 <sothearithkoem@gmail.com>
2026-09-18 07:30:49 +0700

    sidbar is already have responsive

--- resources/views/layout/user.blade.php
+++ resources/views/layout/user.blade.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 </head>
 <body>
-    
+    <div class="flex justify-start">
+        <x-sidbar.user />
+        <main class="md:pl-80">
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
