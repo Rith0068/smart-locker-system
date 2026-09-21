0b70bc2c64af56c4a7477efbab09a27ebda8e378
vannararin23-sketch <vannararin23@gmail.com>
2026-09-21 07:57:22 +0700

    Done login and regisster

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+
+Route::get('/login',[LoginController::class, 'index']);
+
+
+Route::get('/register', [RegisterController::class, 'index']);
 
 Route::get('/', function () {
     return view('index');
